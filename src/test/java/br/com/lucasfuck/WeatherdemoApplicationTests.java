4b94a185046596ec7195ebf11bfd9fafc0ee44a5
Lucas Fuck <lucasdebritof1@gmail.com>
2020-02-05 20:49:24 -0300

    Commit inicial do projeto

---
+++ src/test/java/br/com/lucasfuck/WeatherdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasfuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
